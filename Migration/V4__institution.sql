0c5e2df3ec064eaf4cc8bbcbf52ddb9077273b53
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2021-05-06 08:47:20 +0300

    Update and rename 4_act_institution.sql to V4__institution.sql

--- Migration/V4__institution.sql
+++ Migration/V4__institution.sql
@@ -5,7 +5,7 @@ SELECT DISTINCT ON (allEduInfo.eduName)
 	Location.loc_id,
 	tbl_zno_data.Institution_Parent
 FROM (
-    -- інформація про всі навчальні заклади: назва та місцезнаходження
+   
     select distinct * 
     FROM (
         SELECT DISTINCT EOName, EOTerName, EOAreaName, EORegName FROM tbl_zno_data
@@ -22,10 +22,10 @@ FROM (
         UNION SELECT DISTINCT spaPTName,  spaPTTerName,  spaPTAreaName,  spaPTRegName  FROM tbl_zno_data
     ) as temp
 ) AS allEduInfo (eduName, TerName, AreaName, RegName)
--- join tbl_zno_data, щоб додати інформацію про тип навчального закладу та про EOParent
+
 LEFT JOIN tbl_zno_data ON 
 	allEduInfo.EduName = tbl_zno_data.Institution_Name
--- join Location, щоб додати інформацію про loc_id
+
 LEFT JOIN Location ON
 	allEduInfo.TerName = Location.Territory_Name AND
 	allEduInfo.AreaName = Location.Area AND
